3e4dec417418d6dd8da831c2a65caf9b85a23cd9
Roi Martin <jroi.martin@gmail.com>
2015-01-08 12:18:45 +0100

    Modify flags. Add minoutput.lua
    Flags: Now mandatory flags must be passed as arguments
    minoutput.lua: Lua script that gives a parseable output from tg

--- main.go
+++ main.go
@@ -11,43 +11,52 @@ import (
 	"regexp"
 )
 
-var (
-	tgbin  = flag.String("tgbin", "", "telegram-cli executable")
-	pubkey = flag.String("pubkey", "", "telegram server public key")
-	chat   = flag.String("chat", "", "monitored chat (all if not defined")
-)
+// TODO (jrm): Support multiple channels
+var chat = flag.String("chat", "", "monitored chat (all if not defined)")
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: tgbot [flag] tgbin pubkey minoutput")
+	flag.PrintDefaults()
+}
 
 func main() {
+	flag.Usage = usage
 	flag.Parse()
 
-	if *tgbin == "" || *pubkey == "" {
-		fmt.Fprintln(os.Stderr, "error: -tgbin and -pubkey are mandotory")
-		os.Exit(1)
+	if flag.NArg() != 3 {
+		usage()
+		os.Exit(2)
 	}
+
+	tgbin := flag.Arg(0)
+	pubkey := flag.Arg(1)
+	minoutput := flag.Arg(2)
 
 	// Clean shutdown with Ctrl-C
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
 
-	// -R: disable readline, -C: disable color
-	cmd := exec.Command(*tgbin, "-R", "-C", "-k", *pubkey)
+	// -R: disable readline, -C: disable color, -D: disable output, -s: lua script
+	cmd := exec.Command(tgbin, "-R", "-C", "-D", "-s", minoutput, "-k", pubkey)
 
 	stdoutPipe, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	stdinPipe, err := cmd.StdinPipe()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	/*
+		stdinPipe, err := cmd.StdinPipe()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	*/
 
 	if err := cmd.Start(); err != nil {
 		log.Fatalln(err)
 	}
 
 	// TODO (jrm): multiple commands. !echo is just a POC
-	// Format: "[00:00]  chat nick >>> msg"
-	re := regexp.MustCompile(`^\[\d{2}:\d{2}\]  (.*?) (.*?) >>> !echo (.*)$`)
+	// Format: "title from msg"
+	re := regexp.MustCompile(`^\[MSG\] ([^ ]+) ([^ ]+) (.*)$`)
 
 	log.Println("Monitoring...")
 	s := bufio.NewScanner(stdoutPipe)
@@ -61,11 +70,15 @@ readLoop:
 				break readLoop
 			}
 			line := s.Text()
-			log.Println("DEBUG:", line)
+			//log.Println("DEBUG:", line)
 			sm := re.FindStringSubmatch(line)
-			if len(sm) == 4 && (*chat == "" || *chat == sm[1]) {
-				fmt.Fprintf(stdinPipe, "msg %s Auto: %s\n", sm[1], sm[3])
+			if len(sm) != 4 {
+				continue
 			}
+			title := sm[1]
+			from := sm[2]
+			msg := sm[3]
+			log.Printf("DEBUG: title=%s, from=%s, msg=%s\n", title, from, msg)
 		}
 	}
 	if err := s.Err(); err != nil {
---
+++ minoutput.lua
@@ -0,0 +1,31 @@
+started = 0
+
+function get_title(from, to)
+	if (to.type == 'user') then
+		return from.print_name
+	elseif (to.type == 'chat') then
+		return to.print_name
+	elseif (to.type == 'encr_chat') then
+		return from.print_name
+	else
+		return ''
+	end
+end
+
+function on_msg_receive (msg)
+  if started == 0 then
+    return
+  end
+  print("[MSG] "..get_title(msg.from, msg.to).." "..msg.from.print_name.." "..msg.text)
+end
+
+function on_binlog_replay_end ()
+  started = 1
+end
+
+-- Fix error "*** lua: attempt to call a nil value"
+function on_our_id (id) end
+function on_user_update (user, what) end
+function on_chat_update (chat, what) end
+function on_secret_chat_update (schat, what) end
+function on_get_difference_end () end
