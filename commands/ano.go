8fce0b9cc8908bff082186aa122a7bfc3d5cfe55
Roi Martin <jroi.martin@gmail.com>
2015-01-10 19:51:30 +0100

    commands/ano: refactoring using JSON API, add search by tags

--- commands/ano.go
+++ commands/ano.go
@@ -5,15 +5,21 @@
 package commands
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"path"
 	"regexp"
+	"strings"
 )
+
+var errorNew = errors.New("new file")
 
 type cmdAno struct {
 	name        string
@@ -23,8 +29,7 @@ type cmdAno struct {
 	config      AnoConfig
 
 	// Regexp used to get the pic URL
-	picRegexp *regexp.Regexp
-	picsDir   string
+	tempDir string
 }
 
 type AnoConfig struct {
@@ -33,11 +38,10 @@ type AnoConfig struct {
 
 func NewCmdAno(config AnoConfig) Command {
 	return &cmdAno{
-		syntax:      "!a",
-		description: "Return a random ANO pic",
-		re:          regexp.MustCompile(`^!a$`),
+		syntax:      "!a [tags]",
+		description: "if tags, search ANO by tags (comma-separated). Otherwise return a random pic",
+		re:          regexp.MustCompile(`^!a($| .+$)`),
 		config:      config,
-		picRegexp:   regexp.MustCompile(`/pics/[^"]+`),
 	}
 }
 
@@ -57,89 +61,190 @@ func (cmd *cmdAno) Match(text string) bool {
 	return cmd.re.MatchString(text)
 }
 
+// Shutdown should remove the temp dir on exit.
 func (cmd *cmdAno) Shutdown() error {
-	if cmd.picsDir == "" {
+	if cmd.tempDir == "" {
 		return nil
 	}
-	log.Println("Removing ANO pics dir:", cmd.picsDir)
-	if err := os.RemoveAll(cmd.picsDir); err != nil {
+	log.Println("Removing ANO pics dir:", cmd.tempDir)
+	if err := os.RemoveAll(cmd.tempDir); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (cmd *cmdAno) Run(w io.Writer, title, from, text string) error {
-	// Get pic URL
-	res, err := getResponse("http://ano.lolcathost.org/random.mhtml")
-	if err != nil {
-		fmt.Fprintf(w, "msg %s error: Cannot get pic\n", title)
-		return fmt.Errorf("cannot get pic. failed request to random page")
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		return err
-	}
-	picPath := cmd.picRegexp.FindString(string(body))
-	if picPath == "" {
-		fmt.Fprintf(w, "msg %s error: Cannot get pic\n", title)
-		return fmt.Errorf("cannot get pic. regexp didn't match")
-	}
-	picURL := "http://ano.lolcathost.org" + picPath
+	var (
+		path string
+		err  error
+	)
 
-	// Download pic
-	res, err = getResponse(picURL)
-	if err != nil {
-		fmt.Fprintf(w, "msg %s error: Cannot get pic\n", title)
-		return fmt.Errorf("cannot get pic. failed request to pic URL")
+	tags := strings.TrimSpace(strings.TrimPrefix(text, "!a"))
+	if tags == "" {
+		path, err = cmd.randomPic(title)
+	} else {
+		path, err = cmd.searchTag(title, strings.Split(tags, ","))
 	}
-
-	// Create tmp file
-	if cmd.picsDir == "" {
-		cmd.picsDir, err = ioutil.TempDir("", "tgbot")
-		if err != nil {
-			fmt.Fprintf(w, "msg %s error: Cannot get pic\n", title)
-			return err
-		}
-	}
-
-	var picFile *os.File
-	picTmpPath := path.Join(cmd.picsDir, path.Base(picPath))
-	picFile, err = os.Open(picTmpPath)
 	if err != nil {
-		if !os.IsNotExist(err) {
-			return err
-		}
-		picFile, err = os.Create(picTmpPath)
-		if err != nil {
-			fmt.Fprintf(w, "msg %s error: Cannot get pic\n", title)
-			return err
-		}
-	}
-	defer picFile.Close()
-
-	_, err = io.Copy(picFile, res.Body)
-	res.Body.Close()
-	if err != nil {
-		fmt.Fprintf(w, "msg %s error: Cannot get pic\n", title)
+		fmt.Fprintf(w, "msg %v error: Cannot get pic\n", title)
 		return err
 	}
 
 	// Send to tg as document
-	fmt.Fprintf(w, "msg %s What has been seen cannot be unseen... %s\n", title, picURL)
-	fmt.Fprintf(w, "send_document %s %s\n", title, picFile.Name())
+	fmt.Fprintf(w, "msg %v What has been seen cannot be unseen...\n", title)
+	fmt.Fprintf(w, "send_document %v %v\n", title, path)
 	return nil
 }
 
-func getResponse(URL string) (*http.Response, error) {
+// randomPic returns a random pic from ANO
+func (cmd *cmdAno) randomPic(title string) (path string, err error) {
+	var data struct {
+		Pic struct {
+			ID string
+		}
+	}
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", URL, nil)
+
+	// Get random pic ID
+	methodRandom := strings.NewReader(`{ "method" : "random" }`)
+	req, err := http.NewRequest("POST", "http://ano.lolcathost.org/json/pic.json", methodRandom)
 	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	// Receive data
+	bdata, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return "", err
+	}
+	err = json.Unmarshal(bdata, &data)
+	if err != nil {
+		return "", err
+	}
+
+	// Download pic
+	path, err = cmd.download(data.Pic.ID)
+	if err != nil {
+		return "", fmt.Errorf("cannot download pic")
+	}
+	return path, nil
+}
+
+// searchTag returns a pic from ANO with a given tag.
+func (cmd *cmdAno) searchTag(title string, tags []string) (path string, err error) {
+	var data struct {
+		Pics []struct {
+			ID string
+		}
+	}
+	client := &http.Client{}
+
+	// Get random pic ID
+	searchStr := fmt.Sprintf("{ \"method\" : \"searchRelated\", \"tags\" : [%v], \"limit\" : 10 }",
+		tagsString(tags))
+
+	methodSearch := strings.NewReader(searchStr)
+	req, err := http.NewRequest("POST", "http://ano.lolcathost.org/json/tag.json", methodSearch)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	// Receive data
+	bdata, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return "", err
+	}
+	err = json.Unmarshal(bdata, &data)
+	if err != nil {
+		return "", err
+	}
+
+	rndInt := rand.Intn(len(data.Pics) - 1)
+	rndData := data.Pics[rndInt]
+
+	// Download pic
+	path, err = cmd.download(rndData.ID)
+	if err != nil {
+		return "", fmt.Errorf("cannot download pic")
+	}
+	return path, nil
+}
+
+// tagsString construct a valid string with the tags to be
+// send to ANO's searchRelated method.
+func tagsString(tags []string) string {
+	for i := range tags {
+		tags[i] = fmt.Sprintf("\"%v\"", strings.TrimSpace(tags[i]))
+	}
+	return strings.Join(tags, ",")
+}
+
+// download downloads the pic on the given URL and return
+// the file.
+func (cmd *cmdAno) download(picID string) (path string, err error) {
+	res, err := http.Get("http://ano.lolcathost.org/pics/" + picID)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	f, err := cmd.createTempFile(picID)
+	if err != nil && err != errorNew {
+		return "", err
+	}
+	defer f.Close()
+
+	if err == errorNew {
+		_, err = io.Copy(f, res.Body)
+		if err != nil {
+			return "", err
+		}
+	}
+	return f.Name(), nil
+}
+
+// createTempFile tries to create a new temporary file with
+// the given name. It is important to note that the error
+// will be errorNew if the file did not exist. It also
+// creates a temporary directory in case it has not been
+// created yet.
+func (cmd *cmdAno) createTempFile(filename string) (*os.File, error) {
+	var err, ferr error
+
+	if cmd.tempDir == "" {
+		cmd.tempDir, err = ioutil.TempDir("", "tgbot")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	path := path.Join(cmd.tempDir, filename)
+
+	var f *os.File
+	f, err = os.Open(path)
+	if err != nil && !os.IsNotExist(err) {
 		return nil, err
 	}
-	req.AddCookie(&http.Cookie{
-		Name:  "ANO_PREF",
-		Value: "ip%3D20%26lt%3D0%26ic%3D2%26ml%3D0%26sn%3D0%26it%3D0%26ns%3D0%26md%3D0",
-	})
-	return client.Do(req)
+
+	if os.IsNotExist(err) {
+		f, err = os.Create(path)
+		if err != nil {
+			return nil, err
+		}
+		ferr = errorNew
+	}
+
+	return f, ferr
 }
