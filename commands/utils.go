17f8ed0d790694ec499cfadb38304b8c63009dab
Roi Martin <jroi.martin@gmail.com>
2015-01-12 00:50:37 +0100

    commands/utils: Simplify tempFile()

--- commands/utils.go
+++ commands/utils.go
@@ -5,19 +5,25 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"net/url"
+	"os"
 	"path"
 )
 
-// download downloads the given URL to the directory dstDir in a file with a
-// random name and the extension ext. If ext is "", the file will be created
-// with the same extension of the original file at the given url. It returns the
-// path of the created file. It also creates a temporary directory if dstDir is
-// "".
-func download(dstDir, ext, targetURL string) (filePath string, err error) {
+const alnum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// download downloads the given URL to the directory dir in a file with a random
+// name and the extension ext and returns the path of the created file.
+// If ext is empty string the file will be created with the same extension of
+// the original file at the given url.
+// If dir is the empty string, download uses the default directory for temporary
+// files (see os.TempDir).
+func download(dir, ext, targetURL string) (filePath string, err error) {
 	res, err := http.Get(targetURL)
 	if err != nil {
 		return "", err
@@ -37,7 +43,7 @@ func download(dstDir, ext, targetURL string) (filePath string, err error) {
 		ext = path.Ext(u.Path)
 	}
 
-	f, err := tempFile(dstDir, "", ext)
+	f, err := tempFile(dir, "", ext)
 	if err != nil {
 		return "", nil
 	}
@@ -49,4 +55,43 @@ func download(dstDir, ext, targetURL string) (filePath string, err error) {
 	}
 
 	return f.Name(), nil
+}
+
+// tempFile creates a new temporary file in the directory dir with a name
+// beginning with prefix and ending with suffix, opens the file for reading and
+// writing, and returns the resulting *os.File.
+// If dir is the empty string, tempFile uses the default directory for temporary
+// files (see os.TempDir).
+// The caller can use f.Name() to find the pathname of the file. It is the
+// caller's responsibility to remove the file when no longer needed.
+func tempFile(dir, prefix, suffix string) (*os.File, error) {
+	if dir == "" {
+		dir = os.TempDir()
+	}
+
+	rnd, err := randomStr(32)
+	if err != nil {
+		return nil, err
+	}
+	name := path.Join(dir, prefix+rnd+suffix)
+
+	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
+	if os.IsExist(err) {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+// randomStr returns a random string with the specified length.
+func randomStr(length int) (string, error) {
+	if length <= 0 {
+		return "", errors.New("length must be > 0")
+	}
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = alnum[rand.Intn(len(alnum))]
+	}
+	return string(b), nil
 }
